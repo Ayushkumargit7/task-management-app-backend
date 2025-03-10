85aea680d106f8401d45b30babf4dc4253d95ee0
Ayush Kumar <114018461+Ayushkumargit7@users.noreply.github.com>
2025-03-10 23:59:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = 5000;
@@ -11,10 +13,7 @@ app.use(express.json());
 
 app.use('/tasks', taskRoutes);
 
-mongoose.connect('mongodb://localhost:27017/task-management', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 
